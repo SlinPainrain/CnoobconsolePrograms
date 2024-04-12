2cbff2f10f9944dfa12f70fd02bdebcdedd979f4
Pain Rain <parni@hotmail.de>
2024-04-12 14:45:37 +0200

    yes joar joar, eine globale variable oder so

---
+++ globalscope.c
@@ -0,0 +1,19 @@
+#include stdio.h
+
+
+int result;  global scope
+
+
+void addNumbers(int number1, int number2) {
+    result =  number1 + number2;
+    printf(Result = %d, result);
+}
+
+
+int main() {
+    addNumbers(5,6);
+    
+    
+    return 0;
+    
+}
